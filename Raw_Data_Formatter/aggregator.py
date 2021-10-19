458a9ffbae6174dd8ca606cbe0b7d6c0bcad9fb8
Shashank-Sundi <sundi.sn@gmail.com>
2021-10-19 21:08:38 +0530

    aggreg

--- Raw_Data_Formatter/aggregator.py
+++ Raw_Data_Formatter/aggregator.py
@@ -8,85 +8,83 @@ def aggregate_data():
     try:
 
         Country = request.form['Country']
-        Manufacturing_Site=request.form['Manufacturing Site']
-        Brand=request.form['Brand']
-        Item_Description=request.form['Item Description']
-        Product_Group=request.form['Product Group']
-        Sub_Classification=request.form['Sub Classification']
-        Molecule_Test_Type=request.form['Molecule/Test Type']
-        Dosage_Form=request.form['Dosage Form']
-        Dosage=request.form['Dosage']
-        Managed_By=request.form['Managed By']
-        Fulfill_Via=int(request.form['Fulfill Via'])
-        Shipment_Mode=request.form['Shipment Mode']
+        Manufacturing_Site = request.form['Manufacturing Site']
+        Brand = request.form['Brand']
+        Item_Description = request.form['Item Description']
+        Product_Group = request.form['Product Group']
+        Sub_Classification = request.form['Sub Classification']
+        Molecule_Test_Type = request.form['Molecule/Test Type']
+        Dosage_Form = request.form['Dosage Form']
+        Dosage = request.form['Dosage']
+        Managed_By = request.form['Managed By']
+        Fulfill_Via = int(request.form['Fulfill Via'])
+        Shipment_Mode = request.form['Shipment Mode']
 
-        if request.form['dlvry_delay']=="":
-            dlvry_delay=np.nan
-        else :
-            dlvry_delay=float(request.form['dlvry_delay'])
-
-        if request.form['dlvry_verif_time_delay']=="":
-            dlvry_verif_time_delay=np.nan
+        if request.form['dlvry_delay'] == "":
+            dlvry_delay = np.nan
         else:
-            dlvry_verif_time_delay=float(request.form['dlvry_verif_time_delay'])
+            dlvry_delay = float(request.form['dlvry_delay'])
 
-        if request.form['Unit of Measure (Per Pack)']=="":
-            Unit_of_measure=np.nan
+        if request.form['dlvry_verif_time_delay'] == "":
+            dlvry_verif_time_delay = np.nan
         else:
-            Unit_of_measure=request.form['Unit of Measure (Per Pack)']
+            dlvry_verif_time_delay = float(request.form['dlvry_verif_time_delay'])
 
-        if request.form['Line Item Quantity']=="":
-            Line_Item_Quantity=np.nan
-        else :
-            Line_Item_Quantity=request.form['Line Item Quantity']
-
-        if request.form['Pack Price']=="":
-            Pack_Price=np.nan
+        if request.form['Unit of Measure (Per Pack)'] == "":
+            Unit_of_measure = np.nan
         else:
-            Pack_Price=request.form['Pack Price']
+            Unit_of_measure = request.form['Unit of Measure (Per Pack)']
 
-        if request.form['Unit Price']=="":
-            Unit_Price=np.nan
+        if request.form['Line Item Quantity'] == "":
+            Line_Item_Quantity = np.nan
         else:
-            Unit_Price=request.form['Unit Price']
+            Line_Item_Quantity = request.form['Line Item Quantity']
 
-        First_Line_Designation=int(request.form['First Line Designation'] )
-
-        if request.form['Weight (Kilograms)']=="":
-            Weight=np.nan
+        if request.form['Pack Price'] == "":
+            Pack_Price = np.nan
         else:
-            Weight=request.form['Weight (Kilograms)']
+            Pack_Price = request.form['Pack Price']
 
-        if request.form['Freight Cost (USD)']=="":
-            Freight_Cost=np.nan
+        if request.form['Unit Price'] == "":
+            Unit_Price = np.nan
         else:
-            Freight_Cost=request.form['Freight Cost (USD)']
+            Unit_Price = request.form['Unit Price']
 
-        Freight_cost_special=request.form['Freight_cost_special']
+        First_Line_Designation = int(request.form['First Line Designation'])
 
-        if request.form['Line Item Insurance (USD)']=="":
-            Line_Item_Insurance=np.nan
+        if request.form['Weight (Kilograms)'] == "":
+            Weight = np.nan
         else:
-            Line_Item_Insurance=request.form['Line Item Insurance (USD)']
+            Weight = request.form['Weight (Kilograms)']
 
-        if request.form['Line Item Value']=="":
-            Line_Item_Value=np.nan
+        if request.form['Freight Cost (USD)'] == "":
+            Freight_Cost = np.nan
         else:
-            Line_Item_Value=request.form['Line Item Value']
+            Freight_Cost = request.form['Freight Cost (USD)']
 
-        item_value=(Line_Item_Value+Line_Item_Insurance)/Line_Item_Quantity
+        Freight_cost_special = request.form['Freight_cost_special']
 
+        if request.form['Line Item Insurance (USD)'] == "":
+            Line_Item_Insurance = np.nan
+        else:
+            Line_Item_Insurance = request.form['Line Item Insurance (USD)']
 
+        if request.form['Line Item Value'] == "":
+            Line_Item_Value = np.nan
+        else:
+            Line_Item_Value = request.form['Line Item Value']
 
-
+        item_value = (Line_Item_Value + Line_Item_Insurance) / Line_Item_Quantity
 
         log_writer.log(log_message="Collected inputs from HTML form")
 
-        data = [[age, sex, on_thyroxine, query_on_thyroxine,
-                 on_antithyroid_medication, sick, pregnant, thyroid_surgery,
-                 I131_treatment, query_hypothyroid, query_hyperthyroid, lithium,
-                 goitre, tumor, hypopituitary, psych, TSH, TSH_nan, T3,
-                 T3_nan, TT4, TT4_nan, T4U, FTI, FTI_nan, referral_source]]
+        data = [[Country,Manufacturing_Site,Brand,Item_Description
+                 ,Product_Group,Sub_Classification,Molecule_Test_Type
+                 ,Dosage_Form,Dosage,Managed_By,Fulfill_Via,Shipment_Mode
+                 ,dlvry_delay,dlvry_verif_time_delay,Unit_of_measure
+                 ,Line_Item_Quantity,Pack_Price,Unit_Price,First_Line_Designation
+                 ,Weight,Freight_Cost,Freight_cost_special,Line_Item_Insurance
+                 ,item_value]]
 
         log_writer.log(log_message="Aggregated data inputs from HTML form")
         return data
